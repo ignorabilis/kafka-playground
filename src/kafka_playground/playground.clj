d04eed9896673de81b81b5c67632fed9e26d637c
ignorabilis <irina.yaroslavova@ignorabilis.com>
2018-09-01 16:33:28 +0300

    some exception handling techniques for channels

--- src/kafka_playground/playground.clj
+++ src/kafka_playground/playground.clj
@@ -1,17 +1,36 @@
 (ns kafka-playground.playground
   (:require [kinsky.client :as client]
             [kinsky.async :as async]
-            [clojure.core.async :as a :refer [go <! >!]]))
+            [clojure.core.async :as a :refer [go chan <! >!]]))
+
+(Thread/setDefaultUncaughtExceptionHandler
+ (reify Thread$UncaughtExceptionHandler
+   (uncaughtException [_ thread throwable]
+     (println (.getMessage throwable))
+    ;; handle exception here
+    )))
 
 ;; sample
 (comment
  {:topic "account" :key :account-a :value {:action :logout}})
 
+(def ex-chan (chan))
+
+(def my-topic "my_topic")
 (def ch (async/producer {:bootstrap.servers "localhost:9092"} :keyword :edn))
 
 
 ;; sending a wrong key, i.e. :key 7 (number) blocks the channel
 ;; and further messages are not being produced
+;; exception handling - https://wil.yegelwel.com/Error-Handling-with-Clojure-Async/
 (defn produce-a-message [producer-record]
   (go
-   (>! ch producer-record)))
+    (>! ch producer-record)))
+
+;; Exception handler
+(go
+  (let [e (<! ex-chan)]
+    (println "channel ex handler" )
+    (println (.getMessage e))
+    ;; handle exception
+    ))
