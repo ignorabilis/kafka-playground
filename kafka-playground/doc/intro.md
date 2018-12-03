5fe4217d757e13024b82f1fe0be4062d975b9ba2
ignorabilis <irina.yaroslavova@ignorabilis.com>
2018-12-03 12:27:50 +0200

    Remove duplicate project folder

--- kafka-playground/doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to kafka-playground
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- src/kafka_playground/consumer.clj
+++ src/kafka_playground/consumer.clj
@@ -4,27 +4,31 @@
             [clojure.core.async :as a :refer [go chan <! >!]])
   (:import (java.util UUID)))
 
-(let [[ch ch-2] (async/consumer {:bootstrap.servers "localhost:9092"
-                                 ;:group.id          (str (UUID/randomUUID))
-                                 }
-                                :keyword
-                                :edn
-                                #_(client/keyword-deserializer)
-                                #_(client/edn-deserializer))
-      topic  "my_topic"]
+(def topic "t1")
+(def ch (async/consumer {:bootstrap.servers "localhost:9092"
+                         :group.id          (str (UUID/randomUUID))
+                         :duplex?           true
+                         :topic             topic}
+                        :keyword
+                        :edn))
+
+(let []
   (a/go-loop []
-    (when-let [record (a/<! ch-2)]
+    (when-let [record (a/<! ch)]
       (println (pr-str "GOT!!!" record))
-      (recur)))
-  #_(a/put! ch-2 {:op :partitions-for :topic topic})
-  (a/put! ch {:op :subscribe :topic topic})
-  (a/put! ch {:op :commit})
+      )
+    (recur))
+
+  #_(a/put! ch {:op :partitions-for :topic topic})
+  #_(a/put! ch {:op :subscribe :topic topic})
+  #_(a/put! ch {:op :commit})
   #_(a/put! ch {:op :pause :topic-partitions [{:topic topic :partition 0}
-                                            {:topic topic :partition 1}
-                                            {:topic topic :partition 2}
-                                            {:topic topic :partition 3}]})
+                                              {:topic topic :partition 1}
+                                              {:topic topic :partition 2}
+                                              {:topic topic :partition 3}]})
   #_(a/put! ch {:op :resume :topic-partitions [{:topic topic :partition 0}
-                                             {:topic topic :partition 1}
-                                             {:topic topic :partition 2}
-                                             {:topic topic :partition 3}]})
+                                               {:topic topic :partition 1}
+                                               {:topic topic :partition 2}
+                                               {:topic topic :partition 3}]})
   #_(a/put! ch {:op :stop}))
+
